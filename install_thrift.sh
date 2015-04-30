78aee671a105ea99722691e23ad826d0f1eb033d
Valerio Morsella <valerio.morsella@skyscanner.net>
2015-04-30 10:48:11 +0200

    removing changing permission of sudo

--- install_thrift.sh
+++ install_thrift.sh
@@ -1,5 +1,4 @@
 stat $(which sudo)
-chmod 4111 /usr/bin/sudo
 sudo apt-get update -qq
 sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
 wget http://www.us.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz

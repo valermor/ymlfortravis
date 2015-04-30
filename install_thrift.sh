fe6be67447169c37a54083f9b09a19f9ff021040
Valerio Morsella <valerio.morsella@skyscanner.net>
2015-04-30 17:57:20 +0200

    added ruby to path

--- install_thrift.sh
+++ install_thrift.sh
@@ -1,6 +1,7 @@
 stat $(which sudo)
 which gem
 echo "installing rubygems"
+export PATH=$PATH:/home/travis/.gem/ruby/1.9.1/bin
 /home/travis/.rvm/rubies/ruby-1.9.3-p551/bin/gem install --user-install executable-hooks
 echo "*******************"
 sudo gem install --user-install executable-hooks

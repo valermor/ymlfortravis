6206673ce3040f51f4026955d0933d70a2c35476
Valerio Morsella <valerio.morsella@skyscanner.net>
2015-04-30 10:28:52 +0200

    Added debug log.

--- install_thrift.sh
+++ install_thrift.sh
@@ -1,3 +1,4 @@
+stat $(which sudo)
 sudo apt-get update -qq
 sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
 wget http://www.us.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz

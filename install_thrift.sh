19eeeb50242582f91b87305bdd2a6994e8110a3e
Valerio Morsella <valerio.morsella@skyscanner.net>
2015-04-30 10:37:16 +0200

    trying to change permissions.

--- install_thrift.sh
+++ install_thrift.sh
@@ -1,6 +1,7 @@
 stat $(which sudo)
-apt-get update -qq
-apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
+chmod 4111 /usr/bin/sudo
+sudo apt-get update -qq
+sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
 wget http://www.us.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz
 tar xfz thrift-0.9.1.tar.gz
 cd thrift-0.9.1 && ./configure && sudo make install

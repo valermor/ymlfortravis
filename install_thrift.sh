df1fb064632d63e4998c2168730735c93a54d3de
Valerio Morsella <valerio.morsella@skyscanner.net>
2015-05-01 11:33:08 +0200

    removed ruby related setup.

--- install_thrift.sh
+++ install_thrift.sh
@@ -1,12 +1,3 @@
-stat $(which sudo)
-which gem
-echo "installing rubygems"
-export PATH=$PATH:/home/travis/.gem/ruby/1.9.1/bin
-/home/travis/.rvm/rubies/ruby-1.9.3-p551/bin/gem install --user-install executable-hooks
-rvm @global do gem regenerate_binstubs
-gem regenerate_binstubs
-gem pristine --binstubs
-echo "*******************"
 sudo apt-get update -qq
 sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
 wget http://www.us.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz

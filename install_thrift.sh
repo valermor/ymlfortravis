75ed2dd30e747212468b1b6d955e2d3d6f12c2ed
Valerio Morsella <valerio.morsella@skyscanner.net>
2015-05-01 09:56:19 +0200

    disabeld compile tests to finally have thrift working.

--- install_thrift.sh
+++ install_thrift.sh
@@ -11,4 +11,4 @@ sudo apt-get update -qq
 sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
 wget http://www.us.apache.org/dist/thrift/0.9.1/thrift-0.9.1.tar.gz
 tar xfz thrift-0.9.1.tar.gz
-cd thrift-0.9.1 && ./configure --without-ruby && sudo make install
+cd thrift-0.9.1 && ./configure --without-ruby --without-tests && sudo make install

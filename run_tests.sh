35d660ba7fbba0a0027ced6f3949fd0dc282eb6b
Valerio Morsella <valerio.morsella@skyscanner.net>
2015-05-02 22:12:59 +0200

    removed saucelabs plugin config.

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 cd galen-sample-py-tests
 ls -al
-./scripts/install_requirements.sh 
-./scripts/run_webdriver_tests.sh --plugin src.saucelabs --saucelabs
+./scripts/install_requirements.sh
+./scripts/run_webdriver_tests.sh
